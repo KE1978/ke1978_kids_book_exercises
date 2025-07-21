3adfd2711dd9fc376f6b78ef1bd8ba48ba73aca9
KE1978 <krille.egnell@gmail.com>
2025-07-21 18:38:49 +0200

    Initial commit

---
+++ string_indexing_slicing_practice.py
@@ -0,0 +1,19 @@
+# String indexing & slicing practice
+# Date: 2025-07-20
+
+
+
+# note 2 self "" meaning text — nothing else. outside "" mean: run code."
+
+str = 'programss'
+print('str =', str)
+print("str[0]=", str[0])
+
+print("str[-1]", str[-1])
+#slicing 2nd to 5th character
+print("str[1:5]",str[1:5])
+
+#slicing 6th to 2nd last character
+print("str[5:-2]=" , str[5:-2])
+
+
